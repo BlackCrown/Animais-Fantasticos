e622a1e9610c313cf528e08830614f6047116f71
Wagner Araujo <wagnercorp17@gmail.com>
2024-08-19 16:00:12 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+import initAnimacaoScroll from './modules/scroll-animacao.js'
+import initTabNav from './modules/tabNav.js'
+import initAccordion from './modules/accordion-animacao.js'
+import initScrollSuave from './modules/scroll-suave.js'
+import initModal from './modules/modal.js'
+import initTooltip from './modules/tooltip.js'
+import initDropdownMenu from './modules/dropdown_Menu.js'
+import initMenuMobile from './modules/menu-mobile.js'
+import initAnimaNumeros from './modules/anima-numeros.js'
+import initHorarioFuncionamento from './modules/horarioFuncionamento.js'
+
+
+
+initAnimacaoScroll();
+initTabNav();
+initAccordion();
+initScrollSuave();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initAnimaNumeros();
+initHorarioFuncionamento();
